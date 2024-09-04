871118ee596eb021c8c7ed97b9b2c4d6d2743d68
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-09-04 14:38:44 +0100

    add stolensociety projectcard and page

--- my-portfolio/src/components/featuredprojects.jsx
+++ my-portfolio/src/components/featuredprojects.jsx
@@ -1,6 +1,6 @@
-import * as React from "react";
 import { Box, Typography, Chip, Container } from "@mui/material";
 import pairsniperlogo from "/pairsniperlogo.svg";
+import stolensocietylogoportfolio from "/stolensocietylogoportfolio.png";
 import ncnewslogo from "/northcodersnewslogo.png";
 import { Link } from "react-router-dom";
 
@@ -181,6 +181,134 @@ function FeaturedProjects() {
             ></Chip>
             <Chip
               label="Vercel"
+              variant="outlined"
+              className="text-sm rounded-full p-2 m-1"
+              sx={{
+                "& .MuiChip-label": {
+                  color: "white",
+                },
+                "& .MuiChip-outlined": {
+                  borderColor: "green",
+                },
+              }}
+            ></Chip>
+            <Chip
+              label="Figma"
+              variant="outlined"
+              className="text-sm rounded-full p-2 m-1"
+              sx={{
+                "& .MuiChip-label": {
+                  color: "white",
+                },
+                "& .MuiChip-outlined": {
+                  borderColor: "green",
+                },
+              }}
+            ></Chip>
+            <Chip
+              label="Photoshop"
+              variant="outlined"
+              className="text-sm rounded-full p-2 m-1"
+              sx={{
+                "& .MuiChip-label": {
+                  color: "white",
+                },
+                "& .MuiChip-outlined": {
+                  borderColor: "green",
+                },
+              }}
+            ></Chip>
+          </div>
+        </Box>
+        <Box
+          className="rounded-lg p-5"
+          sx={{
+            boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.8)",
+            width: "35vh",
+            height: "fit-content",
+            minHeight: "490px",
+            maxHeight: "490px",
+            "@media (max-width: 600px)": {
+              minHeight: "auto",
+              maxHeight: "auto",
+            },
+          }}
+        >
+          <Link to="/Stolensociety">
+            <img
+              className="card"
+              src={stolensocietylogoportfolio}
+              alt="stlolensocietylogo"
+              style={{
+                height: "auto",
+                width: "30vh",
+                marginBottom: "10px",
+                flexGrow: "1",
+              }}
+            />
+          </Link>
+          <div
+            className="flex justify-center flex-wrap"
+            style={{
+              flexWrap: "wrap",
+              justifyContent: "space-evenly",
+              width: "100%",
+            }}
+          >
+            <Chip
+              label="Node"
+              variant="outlined"
+              className="text-sm rounded-full p-2 m-1"
+              sx={{
+                "& .MuiChip-label": {
+                  color: "white",
+                },
+                "& .MuiChip-outlined": {
+                  borderColor: "green",
+                },
+              }}
+            ></Chip>
+            <Chip
+              label="React"
+              variant="outlined"
+              className="text-sm rounded-full p-2 m-1"
+              sx={{
+                "& .MuiChip-label": {
+                  color: "white",
+                },
+                "& .MuiChip-outlined": {
+                  borderColor: "green",
+                },
+              }}
+            ></Chip>
+            <Chip
+              label="Shopify"
+              variant="outlined"
+              className="text-sm rounded-full p-2 m-1"
+              sx={{
+                "& .MuiChip-label": {
+                  color: "white",
+                },
+                "& .MuiChip-outlined": {
+                  borderColor: "green",
+                },
+              }}
+            ></Chip>
+            <Chip
+              label="Tailwind"
+              variant="outlined"
+              className="text-sm rounded-full p-2 m-1"
+              sx={{
+                "& .MuiChip-label": {
+                  color: "white",
+                },
+                "& .MuiChip-outlined": {
+                  borderColor: "green",
+                },
+              }}
+            ></Chip>
+            <Chip
+              label="Netlify"
               variant="outlined"
               className="text-sm rounded-full p-2 m-1"
               sx={{
