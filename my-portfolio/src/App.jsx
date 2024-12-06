8e1d79e9d6af25c49fb7c11344e915ff3b30fbce
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-12-06 12:22:51 +0000

    remove cv and name from appbar

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -4,13 +4,11 @@ import Home from "./Home";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import "./App.css";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./theme";
 import PairSniper from "./components/PairSniper";
-import kylepowiscv from "/KYLEPOWISCV2024May.pdf";
 import StolenSociety from "./components/StolenSociety";
 
 function App() {
@@ -32,9 +30,6 @@ function App() {
           style={{ backgroundColor: "rgba(10, 10, 10, 1)" }}
         >
           <Toolbar>
-            <Typography>
-              <span style={{ color: "#FFFFFF" }}>{"Kyle Powis"}</span>
-            </Typography>
             <Box sx={{ flexGrow: 1 }} />
             <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
               <Button
@@ -57,15 +52,6 @@ function App() {
                   ></Button>
                 </>
               )}
-              <Button
-                color="inherit"
-                sx={{ color: "white" }}
-                href={kylepowiscv}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                CV
-              </Button>
             </Box>
           </Toolbar>
         </AppBar>
